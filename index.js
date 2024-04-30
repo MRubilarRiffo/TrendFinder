725def133b7b327317df7c239328b1465354451d
MRubilarRiffo <m.rubilar1010@gmail.com>
2024-04-30 17:18:29 -0400

    Fix index

--- index.js
+++ index.js
@@ -3,17 +3,13 @@ const { conn } = require('./src/infrastructure/config/database');
 const { logMessage } = require('./src/helpers/logMessage');
 const { configCategory } = require('./src/infrastructure/config/configCategory');
 const { configCron } = require('./src/infrastructure/config/configCron');
-const { getSale } = require('./src/handlers/Sale/getSale');
-const { dailySaleReport } = require('./src/infrastructure/cron/dailySaleReport');
 
 const PORT = 3001;
 
 conn.sync({ force: false })
     .then(async () => {
-        // await configCategory();
-        // await configCron();
-
-        // await dailySaleReport();
+        await configCategory();
+        await configCron();
     })
     .then(() => {
         server.listen(PORT, () => logMessage(`Server listening on port ${PORT}`));
