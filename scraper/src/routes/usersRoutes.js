5b15087574661c8a1468a9acac258795248261c2
MRubilarRiffo <m.rubilar1010@gmail.com>
2024-05-10 04:33:03 -0400

    Eliminación de archivos en desuso

--- scraper/src/routes/usersRoutes.js
+++
@@ -1,9 +0,0 @@
-const usersRoutes = require('express').Router();
-
-const { createUser_Controller } = require('../controllers/user/createUser_Controller');
-const { loginUser } = require('../controllers/user/loginUser');
-
-usersRoutes.post('/create', createUser_Controller);
-usersRoutes.post('/login', loginUser);
-
-module.exports = usersRoutes;
